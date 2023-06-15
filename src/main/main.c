9eebc4c46890b8b4cdf3e213774ad0c23a5946ee
crteensy <redecker@avrbeginners.net>
2023-06-15 21:39:12 +0200

    tweak blinky code

--- src/main/main.c
+++ src/main/main.c
@@ -45,17 +45,18 @@ int main(int argc, char * argv[])
     init();
 
     // crteensy
+/*
     GPIO_InitTypeDef GPIO_InitStruct = {0};
-    /*Configure GPIO pin : PA10 */
+    //Configure GPIO pin : PA10 
     GPIO_InitStruct.Pin = GPIO_PIN_8;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
     // /crteensy
-
+*/
 //
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET);
   while(1)
   {
     HAL_Delay(500);
